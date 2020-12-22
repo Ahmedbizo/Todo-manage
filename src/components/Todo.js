1a4234ed87019de0071955204f70090397a5c378
Ahmed Bizo <ahmedmajedb@hotmail.com>
2020-12-22 01:49:44 +0100

    made Todo List

---
+++ src/components/Todo.js
@@ -0,0 +1,13 @@
+import React, {useState} from 'react';
+import TodoForm from './TodoForm';
+
+
+function Todo() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Todo
