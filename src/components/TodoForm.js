4f8f783c6ee9d422574d5d4e5e0ef39abd8e1237
Ahmed Bizo <ahmedmajedb@hotmail.com>
2020-12-22 02:08:15 +0100

    Added styling css

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -17,9 +17,9 @@ function TodoForm(props) {
     e.preventDefault();
 
     props.onSubmit({
-      id: Math.floor(Math.random() * 10000),
-      text: input
-    });
+        id: Math.floor(Math.random() * 10000),
+        text: input
+      });
     setInput('');
   };
 
