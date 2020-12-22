9586b5b310939e23842664c81bf7a85e4f34e3f5
Ahmed Bizo <ahmedmajedb@hotmail.com>
2020-12-22 02:20:17 +0100

    editing and finish the application

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -42,7 +42,7 @@ function TodoList() {
 
   return (
     <>
-      <h1>What's the Plan for Today?</h1>
+      <h1>ToDo Manage</h1>
       <TodoForm onSubmit={addTodo} />
       <Todo
         todos={todos}
