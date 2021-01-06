d51a2997d85e471bc4feb17c487bc9615eee9051
Ahmed Bizo <ahmedmajedb@hotmail.com>
2021-01-06 03:36:24 +0100

    testing TodoList function

--- src/components/TodoList.test.js
+++ src/components/TodoList.test.js
@@ -1,11 +1,19 @@
-
+import React  from 'react';
+import Enzyme, {shallow} from 'enzyme';
+import adapter from 'enzyme-adapter-react-16';
 
 
 describe ('TodoList Component', () => {
 
+
+
+    Enzyme.configure ({
+        adapter: new adapter()
+    });
+
     it ('should render the right List and make editing on it  ', () => {
 
-        expect (true).toBe(true);
+        <shallow/>
 
     })
 })
