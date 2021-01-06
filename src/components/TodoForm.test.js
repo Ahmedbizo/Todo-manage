787295104b55e192dfa898ee31695a725722757a
Ahmed Bizo <ahmedmajedb@hotmail.com>
2021-01-06 03:14:04 +0100

    testing TodoForm and installing enzyme , enzyme adapter

--- src/components/TodoForm.test.js
+++ src/components/TodoForm.test.js
@@ -1,11 +1,28 @@
+import React from 'react';
+import TodoForm from './TodoForm';
+import Enzyme, {shallow} from 'enzyme';
+import adapter from 'enzyme-adapter-react-16';
 
 
 
 describe ('TodoForm Component', () => {
 
+    let component;
+  
+
+    Enzyme.configure ({
+        adapter: new adapter()
+    });
+
     it ('should render the right form  ', () => {
 
-        expect (true).toBe(true);
+        
+
+        component= shallow(
+            <TodoForm />
+        );
+       
+        
 
     })
 })
